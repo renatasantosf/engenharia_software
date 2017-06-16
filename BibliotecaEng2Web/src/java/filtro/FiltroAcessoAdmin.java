2e22bed5baa61f64eccb159a4f90fb8d8c9f11b7
FRANCKE SELAU DIMER <631510072@fspoaeduc.com.br>
2017-06-16 12:54:57 -0300

    filtros

---
+++ BibliotecaEng2Web/src/java/filtro/FiltroAcessoAdmin.java
@@ -0,0 +1,50 @@
+package filtro;
+
+
+import bean.LoginMB;
+import bean.LoginMB;
+import java.io.IOException;
+import javax.inject.Inject;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author lhries
+ */
+@WebFilter(filterName = "FiltroAcessoAdmin", urlPatterns = {"/faces/admin/*"})
+public class FiltroAcessoAdmin implements Filter {
+    @Inject
+    LoginMB loginMB;
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+        System.out.println("Verificando acesso do admin!");
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        //LoginMB loginMB = (LoginMB) req.getSession().getAttribute("loginMB");
+        if(loginMB!=null && loginMB.estaLogado() && loginMB.eAdmin())
+            chain.doFilter(request, response);
+        else
+            resp.sendRedirect(req.getContextPath()+"/faces/login.xhtml");
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+}
