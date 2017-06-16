9ab7180cdd78036ff49888aa609a899458306da0
Renata Fraga <renatysf@gmail.com>
2017-06-16 11:47:39 -0300

    Insercao do projeto em nova branch

--- Biblioteca/src/br/com/senacrs/biblioteca/Biblioteca.java
+++ Biblioteca/src/br/com/senacrs/biblioteca/Biblioteca.java
@@ -8,12 +8,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author renat
@@ -33,4 +27,5 @@ public class Biblioteca extends Application  {
     public static void main(String[] args) {
         launch(args);
     }
+    
 }
