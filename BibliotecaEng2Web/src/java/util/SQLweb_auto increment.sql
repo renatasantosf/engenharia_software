6ff27314ec52b6911857f69792696cb89a9181ab
FRANCKE SELAU DIMER <francke.selau@email.com>
2017-06-30 11:47:36 -0300

    auto incremento sql

---
+++ BibliotecaEng2Web/src/java/util/SQLweb_auto increment.sql
@@ -0,0 +1,4 @@
+﻿CREATE SEQUENCE livro_id_seq;
+ALTER TABLE livro ALTER COLUMN id SET NOT NULL;
+ALTER TABLE livro ALTER COLUMN id SET DEFAULT nextval('livro_id_seq');
+ALTER SEQUENCE livro_id_seq OWNED BY livro.id;
